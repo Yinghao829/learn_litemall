6c34d04c28f9da31c976605b943dc0569c08778a
Hao <yinghaoL829@outlook.com>
2025-05-13 15:25:14 +0800

    first commit

---
+++ litemall_admin_api/src/main/java/org/hao/LitemallAdminApiApplication.java
@@ -0,0 +1,13 @@
+package org.hao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitemallAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitemallAdminApiApplication.class, args);
+    }
+
+}
---
+++ litemall_all/src/main/java/org/hao/LitemallAllApplication.java
@@ -0,0 +1,13 @@
+package org.hao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitemallAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitemallAllApplication.class, args);
+    }
+
+}
